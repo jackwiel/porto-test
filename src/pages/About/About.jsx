33d01d5cfea60dbc342518e872eebf1011e2d08a
Akira Dev <louvretwentytwo@gmail.com>
2024-10-04 19:26:01 +0700

    Initial Files

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,75 @@
+import { Canvas } from "@react-three/fiber";
+import gsap from "gsap";
+import Flip from "gsap/Flip";
+import React, { useEffect } from "react";
+import { Page } from "../../components/Page";
+import { blue, green, yellow } from "../../utils";
+import { Educations, Paragraph, SkillsWrapper, Text } from "./About.styled";
+import { AboutItem } from "./AboutItem";
+import Skills from "./SkillBall";
+import dyp from "../../assets/images/dyp.png";
+import highschool from "../../assets/images/highschool.jpeg";
+import sos from "../../assets/images/sos.png";
+import { useInView } from "react-intersection-observer";
+import { useState } from "react";
+
+export const About = () => {
+  const { ref, inView } = useInView({});
+  const [show, setShow] = useState(inView);
+  useEffect(() => {
+    setShow(inView);
+  }, [inView]);
+
+  useEffect(() => {
+    gsap.registerPlugin(Flip);
+    let cards = document.querySelectorAll(".about-item");
+    cards.forEach((card, i) => {
+      if (i === 0) {
+        card.classList.add("active");
+      }
+      card.addEventListener("mouseenter", (e) => {
+        if (card.classList.contains("active")) {
+          return;
+        }
+        const state = Flip.getState(cards);
+        cards.forEach((c) => {
+          c.classList.remove("active");
+        });
+        card.classList.add("active");
+        Flip.from(state, {
+          duration: 0.5,
+          ease: "elastic.out(1,0.9)",
+          absolute: true,
+        });
+      });
+    });
+  }, []);
+  return (
+    <div ref={ref}>
+      <Page header="About">
+        <Text>
+          <Paragraph>
+            I'm a passionate programmer, who is always looking for new
+            challenges to improve myself, also a team player, who is always
+            ready to learn new things and help others.
+            <br />I was born and raised in Subang, Indonesia. I love to play badminton 
+            and in my free time I like to read books, watch movies
+            and play video games.
+          </Paragraph>
+          <Educations>
+            
+          </Educations>
+        </Text>
+        <SkillsWrapper>
+          {show ? (
+            <Canvas camera={{ position: [0, 0, 18] }}>
+              <Skills />
+            </Canvas>
+          ) : (
+            `${inView}`
+          )}
+        </SkillsWrapper>
+      </Page>
+    </div>
+  );
+};
